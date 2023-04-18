70a2bf0173d4ef855333a75819b247192b8936d0
cbroa <charlotte.broad1@nhs.net>
2023-04-18 07:53:54 +0100

    adding some routing

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,24 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//////v1 routing/////////
+
+router.get(/location-england/, function (req, res) {
+    if (req.query.location == 'yes') {
+      res.redirect('date-of-loss');
+    }
+     else if (req.query.location == 'no') {
+     res.redirect('location-kickout');;
+     }
+   });
+  
+
+   router.get(/baby-gender/, function (req, res) {
+    if (req.query.gender == 'yes') {
+      res.redirect('enter-baby-gender');
+    }
+     else if (req.query.gender == 'no') {
+     res.redirect('babys-name');;
+     }
+   });
