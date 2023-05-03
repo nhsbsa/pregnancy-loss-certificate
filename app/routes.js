138d0393961a0d17caee70e9b85bacfac1a33c89
cbroa <charlotte.broad1@nhs.net>
2023-05-03 13:14:20 +0100

    version 2 and kickout placeholders

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,10 @@ const router = govukPrototypeKit.requests.setupRouter()
 ////////   Versions   ////////
 //////////////////////////////
 
+
+router.use('/v2', require('./views/v2/_routes'));
 router.use('/v1', require('./views/v1/_routes'));
+
 
 
 
