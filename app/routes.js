b1c1748ffd7127c9ca7a14b0ab8c772640ef0e1f
cbroa <charlotte.broad1@nhs.net>
2023-05-30 16:02:50 +0100

    version 4 updates

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,8 @@ const router = govukPrototypeKit.requests.setupRouter()
 ////////   Versions   ////////
 //////////////////////////////
 
+
+router.use('/v4', require('./views/v4/_routes'));
 router.use('/v3', require('./views/v3/_routes'));
 router.use('/v2', require('./views/v2/_routes'));
 router.use('/v1', require('./views/v1/_routes'));
