29e641344d44ae4470248a39b1f959bd608dcb3b
cbroa <charlotte.broad1@nhs.net>
2023-06-02 14:18:49 +0100

    ccs journey

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 ////////   Versions   ////////
 //////////////////////////////
 
-
+router.use('/ccs', require('./views/v4/_routes'));
 router.use('/v4', require('./views/v4/_routes'));
 router.use('/v3', require('./views/v3/_routes'));
 router.use('/v2', require('./views/v2/_routes'));
