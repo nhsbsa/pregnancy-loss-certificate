478523e4858bf6529fd67881ba67d0f780138598
Matt Fielding <matt@hippodigital.co.uk>
2023-12-21 11:40:32 +0000

    add crm filters

--- app/filters.js
+++ app/filters.js
@@ -19,3 +19,29 @@ addFilter('formatUKPostcode', function (content) {
   // If it doesn't match the pattern, return the original content
   return content;
 });
+
+addFilter('uppercase', function (content) {
+  return content.toUpperCase()
+})
+
+// filter to turn status text into a tage
+addFilter('tag', function (content) {
+  let colour = 'grey'
+  if (content === 'Incomplete') {
+    colour = 'blue'
+  } else if (content === 'Sent') {
+    colour = 'green'
+  } else if (content === 'Rejected' || content === 'Failed') {
+    colour = 'red'
+  } else if (content === 'Expired') {
+    colour = 'red'
+  }
+  let tagCode = '<strong class="govuk-tag govuk-tag--'+ colour +'">' + content + '</strong>'
+  return tagCode
+})
+
+// swap hypens for slashes in date output
+addFilter('changeDateFormat', function (dateString) {
+  const match = dateString.match(/^(\d{4})-(\d{2})-(\d{2})$/);
+  return match ? `${match[3]}/${match[2]}/${match[1]}` : dateString;
+})
